96f261ad0a7c8943c5b2879651319a5bc9b16985
ozturkmtn <Hb%R753jM>
2021-02-07 09:26:03 +0300

    laravel 1

---
+++ resources/views/fuck.blade.php
@@ -0,0 +1,7 @@
+<h1>{{ in_array('a',['a','ass']) }}</h1>
+
+@if(in_array('a',['a','ass']))
+
+    <h2>AssHOLE</h2>
+
+@endif
---
+++ routes/web.php
@@ -0,0 +1,26 @@
+<?php
+
+use Illuminate\Support\Facades\Route;
+
+/*
+|--------------------------------------------------------------------------
+| Web Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register web routes for your application. These
+| routes are loaded by the RouteServiceProvider within a group which
+| contains the "web" middleware group. Now create something great!
+|
+*/
+
+Route::get('/sample', function () {
+    return view('fuck');
+});
+
+Route::get('/f/{id}', function ($id){
+    echo $id;
+    return view('welcome');
+    //return redirect('/sample');
+});
+
+Route::redirect('/sample2','/f/302',302);
