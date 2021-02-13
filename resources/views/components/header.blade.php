95f306ae5146734c92bafd4cbaf6ba702a6d9abc
ozturkmtn <Hb%R753jM>
2021-02-13 12:34:42 +0300

    component sample second part.

---
+++ resources/views/components/header.blade.php
@@ -0,0 +1,7 @@
+<div>
+    <!-- I begin to speak only when I am certain what I will say is not better left unsaid - Cato the Younger -->
+
+    <h1> {{$title ?? ''}} Header Component</h1>
+
+    <h3>{{$info}}</h3>
+</div>
